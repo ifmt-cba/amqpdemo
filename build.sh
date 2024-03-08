f654904eab9b110f85758eaa146352e02a723a65
João Paulo <jppreti@gmail.com>
2024-03-08 08:18:42 -0400

    ACK Manual Implementado

--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@ mvn clean package
 mv ./target/*.jar ./target/app.jar
 
 docker build -t br.edu.ifmt/amqpdemo --no-cache .
+docker rmi $(docker images -f "dangling=true" -q)
--- src/main/java/br/edu/ifmt/amqpdemo/consumidor/Carla.java
+++ src/main/java/br/edu/ifmt/amqpdemo/consumidor/Carla.java
@@ -1,16 +1,23 @@
 package br.edu.ifmt.amqpdemo.consumidor;
 
+import java.io.IOException;
+
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.rabbitmq.client.Channel;
 
 @Service
 @Transactional
 public class Carla {
 
-    @RabbitListener(queues = "q.carla")
-    private void recebeMensagem(String msg) {
+    @RabbitListener(queues = "q.carla", ackMode = "MANUAL")
+    private void recebeMensagem(String msg, Channel channel, @Header(AmqpHeaders.DELIVERY_TAG) long tag) throws IOException {
         System.out.println("Carla recebeu: " + msg);
+        channel.basicAck(tag, false);
     }
 
 }
